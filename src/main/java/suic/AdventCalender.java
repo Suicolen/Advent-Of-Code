59633d5eaa2e755f1f2bc645a4b697ad3c48041a
Suicolen <ristogamimg1212@gmail.com>
2020-12-04 12:02:27 +0200

    Day 4, regex is fun, dumb puzzle
    gotta refactor

--- src/main/java/suic/AdventCalender.java
+++ src/main/java/suic/AdventCalender.java
@@ -1,26 +1,15 @@
 package suic;
 
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class AdventCalender {
 
     private static final String BASE_PACKAGE = AdventCalender.class.getPackageName() + ".days.";
 
     public static void main(String[] args) {
-        run(1);
+        runAll();
     }
 
     private static void run() {
-      //runAll();
+      runAll();
     }
 
     private static void run(int dayOfMonth) {
---
+++ src/main/java/suic/days/day4/Day4.java
@@ -0,0 +1,79 @@
+package suic.days.day4;
+
+import suic.Puzzle;
+import suic.util.FileUtils;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Day4 implements Puzzle<String, Long> {
+    
+    @Override
+    public List<String> parse() {
+        List<String> input = FileUtils.readResource(getClass().getSimpleName() + "Input.txt");
+        List<String> passports = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (String line : input) {
+            if (!line.isEmpty()) {
+                sb.append(line).append(" ");
+            } else {
+                passports.add(sb.toString().trim());
+                sb.setLength(0);
+            }
+        }
+        return passports;
+    }
+
+
+    @Override
+    public Long solvePart1() {
+        return parse().stream().filter(this::isValidPart1).count();
+    }
+
+    private boolean isValidPart1(String str) {
+        int count = str.contains("cid:") ? 8 : 7;
+        return str.split(" ").length == count;
+    }
+
+
+    @Override
+    public Long solvePart2() {
+        long count = 0;
+        List<String> passports = parse();
+        for (String passport : passports) {
+            String[] parts = passport.split(" ");
+            if (parts.length < 7) { // invalid in the beginning?
+                continue;
+            }
+
+            parts = Arrays.stream(parts).filter(s -> !s.startsWith("cid")).toArray(String[]::new);
+
+            if (parts.length < 7) { // invalid after filtering?
+                continue;
+            }
+            Arrays.sort(parts, Comparator.comparing(s -> s.substring(0, 3)));
+            if (part2Valid(parts)) {
+                count++;
+            }
+
+
+        }
+        return count;
+    }
+
+    final String[] REGEXES = {
+            "^(19[2-9]\\d|200[012])$",
+            "(amb|blu|brn|gry|grn|hzl|oth)",
+            "(202\\d)|(2030)",
+            "#([a-fA-F0-9]{6})",
+            "^((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)$",
+            "(201\\d)|(2020)",
+            "\\d{9}"
+    };
+
+
+    private boolean part2Valid(String[] passports) {
+        return IntStream.range(0, passports.length).allMatch(i -> passports[i].split(":")[1].matches(REGEXES[i]));
+    }
+
+}
