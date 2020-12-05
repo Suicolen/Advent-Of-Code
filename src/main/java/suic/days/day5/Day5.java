b4dc1deee6ceeb4a876d4cfb625dcdf8e391cfa5
Suicolen <ristogamimg1212@gmail.com>
2020-12-05 09:35:11 +0200

    Refactoring a bit
    ya

--- src/main/java/suic/days/day5/Day5.java
+++ src/main/java/suic/days/day5/Day5.java
@@ -3,33 +3,29 @@ package suic.days.day5;
 import suic.Puzzle;
 import suic.util.FileUtils;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Day5 implements Puzzle<String, Integer> {
+public class Day5 implements Puzzle<Integer, Integer> {
+
     @Override
-    public List<String> parse() {
-        return FileUtils.readResource(getClass().getSimpleName() + "Input.txt");
+    public List<Integer> parse() {
+        return FileUtils.readResource(getClass().getSimpleName() + "Input.txt").stream().map(line -> line
+                .replace("F", "0").
+                        replace("B", "1").
+                        replace("L", "0").
+                        replace("R", "1")).mapToInt(b -> Integer.parseInt(b, 2)).boxed().collect(Collectors.toList());
     }
 
     @Override
     public Integer solvePart1() {
-        return parse().stream().map(line -> line
-                .replace("F", "0").
-                        replace("B", "1").
-                        replace("L", "0").
-                        replace("R", "1")).mapToInt(b -> Integer.parseInt(b, 2)).max().orElse(-1);
+        return parse().stream().max(Comparator.naturalOrder()).orElse(-1);
     }
 
     @Override
     public Integer solvePart2() {
-        List<String> input = parse();
-        List<Integer> seatIds = input.stream().map(line -> line
-                .replace("F", "0").
-                        replace("B", "1").
-                        replace("L", "0").
-                        replace("R", "1")).mapToInt(b -> Integer.parseInt(b, 2)).boxed().sorted()
-                .collect(Collectors.toList());
+        List<Integer> seatIds = parse().stream().sorted().collect(Collectors.toList());
         int first = seatIds.get(1);
         int last = seatIds.get(seatIds.size() - 1);
         int mySeatId = -1;
