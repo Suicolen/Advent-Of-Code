2ba4557c7c24b56bb3368f87e1c9c44d74c965e7
Suicolen <ristogamimg1212@gmail.com>
2020-12-05 12:38:36 +0200

    Refactoring
    ye

--- src/main/java/suic/days/day5/Day5.java
+++ src/main/java/suic/days/day5/Day5.java
@@ -4,36 +4,49 @@ import suic.Puzzle;
 import suic.util.FileUtils;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day5 implements Puzzle<Integer, Integer> {
 
     @Override
     public List<Integer> parse() {
-        return FileUtils.readResource(getClass().getSimpleName() + "Input.txt").stream().map(line -> line
-                .replace("F", "0").
-                        replace("B", "1").
-                        replace("L", "0").
-                        replace("R", "1")).mapToInt(b -> Integer.parseInt(b, 2)).boxed().collect(Collectors.toList());
+        return FileUtils.readResource(getClass().getSimpleName() + "Input.txt")
+                .stream()
+                .map(line -> line.replace("F", "0")
+                        .
+                                replace("B", "1")
+                        .
+                                replace("L", "0")
+                        .
+                                replace("R", "1"))
+                .mapToInt(b -> Integer.parseInt(b, 2))
+                .boxed()
+                .collect(Collectors.toList());
     }
+
 
     @Override
     public Integer solvePart1() {
-        return parse().stream().max(Comparator.naturalOrder()).orElse(-1);
+        return parse().stream()
+                .max(Comparator.naturalOrder())
+                .orElse(-1);
     }
 
     @Override
     public Integer solvePart2() {
-        List<Integer> seatIds = parse().stream().sorted().collect(Collectors.toList());
-        int first = seatIds.get(1);
-        int last = seatIds.get(seatIds.size() - 1);
-        int mySeatId = -1;
-        for (int i = first; i < last; i++) {
-            if (!seatIds.contains(i)) {
-                mySeatId = i;
-            }
-        }
-        return mySeatId;
+        Set<Integer> seatIds = new HashSet<>(parse());
+        int start = IntStream.range(1, seatIds.size() - 1)
+                .filter(seatIds::contains)
+                .findFirst()
+                .orElse(seatIds.size());
+
+        return IntStream.range(start, start + seatIds.size())
+                .filter(i -> !seatIds.contains(i))
+                .findFirst()
+                .orElse(-1);
     }
 }
